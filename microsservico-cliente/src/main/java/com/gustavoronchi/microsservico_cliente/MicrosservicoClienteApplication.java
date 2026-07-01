34e96cd7a1c5643b3ae8b90a9b0c19c3a436479f
Gustavo Caçador <gustavoirl456@outlook.com>
2026-07-01 01:40:40 -0300

    criando microsservico de cliente

---
+++ microsservico-cliente/src/main/java/com/gustavoronchi/microsservico_cliente/MicrosservicoClienteApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoClienteApplication.class, args);
+	}
+
+}
---
+++ microsservico-cliente/src/test/java/com/gustavoronchi/microsservico_cliente/MicrosservicoClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
