f5d1539b3740235e298c308626b5a7a0e0812201
Gustavo Caçador <gustavoirl456@outlook.com>
2026-07-07 23:05:57 -0300

    incluindo gateway na arquitetura

---
+++ microsservico-gateway/src/main/java/com/gustavoronchi/microsservico_gateway/MicrosservicoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoGatewayApplication.class, args);
+	}
+
+}
---
+++ microsservico-gateway/src/test/java/com/gustavoronchi/microsservico_gateway/MicrosservicoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoronchi.microsservico_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
